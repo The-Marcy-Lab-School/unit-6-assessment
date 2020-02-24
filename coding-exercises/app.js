aae04a290541df387776a055e29b1d1194de1a67
Seal125 <stephaniesmith12514@gmail.com>
2020-02-24 14:18:48 +0000

    code exercises completed

--- coding-exercises/app.js
+++ coding-exercises/app.js
@@ -6,6 +6,12 @@ window.addEventListener('load', () => {
 	 * Define your variables below this comment.
 	 */
 
+	const city = document.getElementsByClassName('city');
+	const iconAnimate = document.getElementsByClassName('icon-animated');
+	const temp = document.getElementsByClassName('temp');
+	const degree = document.getElementsByClassName('degree-type');
+	const description = document.getElementsByClassName('weather-desc');
+
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(position => {
 			console.log('My General Position:', position);
@@ -17,16 +23,41 @@ window.addEventListener('load', () => {
 			 * Make sure to include error handling.
 			 */
 
+			fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/2e16100c34900daeb2d5cadf60f88682/${lat},${long}`).then(response => response.json())
+				.then(data => {
+					temp[0].innerText = `Temperature: ${data.currently.temperature}`;
+					description[0].innerText = `Description: ${data.currently.summary}`;
+					city[0].innerText = `Location: ${data.timezone}`;
+					let icon = data.currently.icon
+					setIcons(icon, iconAnimate[0])
+				}).catch(err => console.warn(err))
+
 			/*TODO: Set the weather icon */
+
 
 			/**TODO: Add an event listener that toggles between Fahrenheit and Celcius
 			 * when a user clicks on the current temperature section.
 			 */
+			temp[0].addEventListener('click', () => {
+				let currTemp = Number(temp[0].innerText.substring(13))
+				if (degree[0].innerText === 'F') {
+					temp[0].innerText = `Temperature: ${Math.floor((currTemp - 32) * (5 / 9))}`
+					degree[0].innerText = "C"
+				}
+				else {
+					temp[0].innerText = `Temperature: ${Math.floor((currTemp * (9 / 5)) + 32)}`
+					degree[0].innerText = "F"
+				}
+			});
 		});
 	}
 
 	/**
 	 * TODO: Code out the remainder of `setIcons`function.
 	 */
-	const setIcons = () => {};
+	const setIcons = (icon, iconID) => {
+		let skycons = new Skycons();
+		skycons.add(iconID, icon);
+		skycons.play();
+	};
 });
