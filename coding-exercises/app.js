5f7e3bd3bf9c8419121d16ad2e7338f854b9a85c
Seal125 <stephaniesmith12514@gmail.com>
2020-02-24 17:24:58 +0000

    complete code and short responses, currently revising

--- coding-exercises/app.js
+++ coding-exercises/app.js
@@ -1,11 +1,4 @@
 window.addEventListener('load', () => {
-	/**
-	 * TODO : Create your 5 variables to get elements
-	 * based on the classes you created in the `index.html`.
-	 *
-	 * Define your variables below this comment.
-	 */
-
 	const city = document.getElementsByClassName('city');
 	const iconAnimate = document.getElementsByClassName('icon-animated');
 	const temp = document.getElementsByClassName('temp');
@@ -13,48 +6,40 @@ window.addEventListener('load', () => {
 	const description = document.getElementsByClassName('weather-desc');
 
 	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(position => {
+		navigator.geolocation.getCurrentPosition(async position => {
 			console.log('My General Position:', position);
 			const long = position.coords.longitude;
 			const lat = position.coords.latitude;
 
-			/* TODO: Continue your fetch request to set the DOM Elements for
-			 * temperature, description/summary, and timezone.
-			 * Make sure to include error handling.
-			 */
+			try {
+				let response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/2e16100c34900daeb2d5cadf60f88682/${lat},${long}`)
+				let data = await response.json()
+				temp[0].innerText = `Temperature: ${data.currently.temperature}`;
+				description[0].innerText = `Description: ${data.currently.summary}`;
+				city[0].innerText = `Location: ${data.timezone}`;
+				let icon = data.currently.icon
+				const fTemp = data.currently.temperature
+				setIcons(icon, iconAnimate[0])
 
-			fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/2e16100c34900daeb2d5cadf60f88682/${lat},${long}`).then(response => response.json())
-				.then(data => {
-					temp[0].innerText = `Temperature: ${data.currently.temperature}`;
-					description[0].innerText = `Description: ${data.currently.summary}`;
-					city[0].innerText = `Location: ${data.timezone}`;
-					let icon = data.currently.icon
-					setIcons(icon, iconAnimate[0])
-				}).catch(err => console.warn(err))
-
-			/*TODO: Set the weather icon */
-
-
-			/**TODO: Add an event listener that toggles between Fahrenheit and Celcius
-			 * when a user clicks on the current temperature section.
-			 */
-			temp[0].addEventListener('click', () => {
-				let currTemp = Number(temp[0].innerText.substring(13))
-				if (degree[0].innerText === 'F') {
-					temp[0].innerText = `Temperature: ${Math.floor((currTemp - 32) * (5 / 9))}`
-					degree[0].innerText = "C"
-				}
-				else {
-					temp[0].innerText = `Temperature: ${Math.floor((currTemp * (9 / 5)) + 32)}`
-					degree[0].innerText = "F"
-				}
-			});
+				temp[0].addEventListener('click', () => {
+					const fTemp = Math.floor(data.currently.temperature)
+					if (degree[0].innerText === 'F') {
+						temp[0].innerText = `Temperature: ${Math.floor((fTemp - 32) * (5 / 9))}`
+						degree[0].innerText = "C"
+						console.log(fTemp)
+					}
+					else {
+						temp[0].innerText = `Temperature: ${fTemp}`
+						degree[0].innerText = "F"
+					}
+				});
+			}
+			catch (error) {
+				console.warn(error)
+			}
 		});
 	}
 
-	/**
-	 * TODO: Code out the remainder of `setIcons`function.
-	 */
 	const setIcons = (icon, iconID) => {
 		let skycons = new Skycons();
 		skycons.add(iconID, icon);
--- short-response/README.md
+++ short-response/README.md
@@ -1,9 +1,13 @@
 ## Short Response Questions:
 
 1. Why is asynchronous code such a fundamental part of programming in JavaScript?
+- It's important because it improves the user experience and allows them to do other things as data is being requested or posted. If it were synchronous, you would have to wait for data to get back to you, which could take a while depending on how much you're requesting.
 
 2. What is a Promise? When were Promises made available in JavaScript? What did they replace and why do they represent an improvement on the previous way asynchronous functions were handled?
+- A Promise is an object in JS that represents the completion or rejection of an asynchronous function or operation with statuses, such as pending, rejected, or fulfilled. They were officially made available in ECMAScript 2015, but as concepts, they've been around for quite a long time. They replaced having to make XMLHttpRequests, which if you wanted to use for a lot of callbacks, would generally look like one big mess that would be hard to read and hard to debug or even identify what's happening at that moment.
 
 3. What are `async` functions? What is their relationship to Promises? What improvements does `async`/`await` introduce?
+- `Async` functions are functions that are labeled as asynchronous, and returns a Promise. It's much like using Promises with then and catch methods, but more syntactically sugared. It makes it easier to read, and you can store data into variables. It also allows you to use the `await` method, which is basically a then method.
 
 4. Consider one of your favorite apps. Describe one feature of that app that is likely powered by AJAX.
+- Steam, which is a game store for PC games, I believe has some sort of AJAX implemented. When you buy a game and it's downloading and installing onto your computer, you're able to browse through the store while the game is downloading. Even in CodeWars, when you click a link to a new page, it'll show you the status of how much it's loaded, and still allow you to go through the page you're currently on and do whatever it is you can do there (such as voting for a solution as clever or best practice).
